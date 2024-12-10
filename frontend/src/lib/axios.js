987052a9a75f6670023d7df92e55e482ed1968fa
omar ayman <110986441+omaar28@users.noreply.github.com>
2024-12-10 19:06:13 +0200

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
+	baseURL: "https://cloud-porject.vercel.app/api",
 	withCredentials: true, // send cookies to the server
 });
 
