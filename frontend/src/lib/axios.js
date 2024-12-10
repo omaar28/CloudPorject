def6189a5c7fe059b420d8843acf327801ea2efb
omar ayman <110986441+omaar28@users.noreply.github.com>
2024-12-10 19:00:45 +0200

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.env.VITE_BACKEND_API + "/api",
+	baseURL: baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
 	withCredentials: true, // send cookies to the server
 });
 
